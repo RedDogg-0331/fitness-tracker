4672d06b6c7ac353b17dd6fc0265829a95331592
Mike <michaelredding@ymail.com>
2020-04-12 11:33:52 -0500

    Stopping for the day

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
     imports: [MatButtonModule,
@@ -14,13 +15,15 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
         MatFormFieldModule,
         MatDatepickerModule,
         MatNativeDateModule,
-        MatCheckboxModule],
+        MatCheckboxModule,
+        MatSidenavModule],
     exports: [MatButtonModule,
         MatIconModule,
         MatInputModule,
         MatFormFieldModule,
         MatDatepickerModule,
         MatNativeDateModule,
-        MatCheckboxModule]
+        MatCheckboxModule,
+        MatSidenavModule]
 })
 export class MateralModule { }
